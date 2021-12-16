7213e652f7b2abaf4eb0acf328c06f5810cb31a8
ansanida <33883725+ansanida@users.noreply.github.com>
2021-12-16 15:21:38 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,8 +1,14 @@
 library(shiny)
+library(leaflet)
 fluidPage(
   titlePanel("Median Income Level in Public College Towns"),
     mainPanel(
-      leafletOutput("distPlot")
+      hr(),
+      radioButtons("map_choice", "Type of College",
+                   choiceNames = list("Public Colleges", "Private Colleges"),
+                   choiceValues = list("Public Colleges", "Private Colleges")),
+      leafletOutput('distPlot')
+      
+      )
+  
     )
-  )
-
