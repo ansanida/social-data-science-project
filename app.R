a306b53a5c31affd8c50b67c610209d9100424c3
ansanida <33883725+ansanida@users.noreply.github.com>
2021-12-16 15:20:10 -0500

    Update app.R

--- app.R
+++ app.R
@@ -1,6 +1,5 @@
 library(shiny)
 library(rsconnect)
-rsconnect::setAccountInfo(name='nida-ansari', 
-                          token='73F99B1C41C2BAAAA30CB58BC8495D7B', 
-                          secret='Rf1Ali452u1Ul1SYyF1MDCldlZ1MbgOGeqe938Sw')
+getwd()
+setwd("/Users/nidaansari/Desktop/sds_project")
 deployApp()
